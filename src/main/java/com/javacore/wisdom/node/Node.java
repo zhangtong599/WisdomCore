0f69910f108c84e6832cd7eb659a6533b9b9e72d
mystudygogo <tnix_blockchain@outlook.com>
2019-03-22 17:06:46 +0800

    add property

--- src/main/java/com/javacore/wisdom/node/Node.java
+++ src/main/java/com/javacore/wisdom/node/Node.java
@@ -1,37 +1,17 @@
 package com.javacore.wisdom.node;
 
 public class Node {
+
+	private   String protocol;
 	
-	private String IPAddress;
+	private   String IPAddress;
 	
-	private int port;
+	private   int port;
 	
 	private int rpcport;
 
-	public String getIPAddress() {
-		return IPAddress;
-	}
 
-	public void setIPAddress(String iPAddress) {
-		IPAddress = iPAddress;
-	}
 
-	public int getPort() {
-		return port;
-	}
-
-	public void setPort(int port) {
-		this.port = port;
-	}
-
-	public int getRpcport() {
-		return rpcport;
-	}
-
-	public void setRpcport(int rpcport) {
-		this.rpcport = rpcport;
-	}
-	
 	
 
 }
