662a4d32ed0cf6bbbadd120e356c162bf8d042e7
mystudygogo <tnix_blockchain@outlook.com>
2019-03-22 17:00:01 +0800

    init StringUtil

---
+++ src/main/java/com/javacore/wisdom/util/StringUtil.java
@@ -0,0 +1,12 @@
+package com.javacore.wisdom.util;
+
+import org.apache.log4j.Logger;
+
+public final class StringUtil {
+	
+	private  Logger logger=Logger.getLogger(StringUtil.class);
+	
+	
+	
+
+}
