443f320a881df11d8df8effb6fcd859f5b9597fe
mystudygogo <tnix_blockchain@outlook.com>
2019-03-22 23:24:50 +0800

    modify sha3512 invoke

--- src/test/java/com/javacore/wisdom/crypto/KeccakTests.java
+++ src/test/java/com/javacore/wisdom/crypto/KeccakTests.java
@@ -1,10 +1,12 @@
 package com.javacore.wisdom.crypto;
 
 import org.apache.commons.codec.binary.Hex;
+import org.bouncycastle.crypto.digests.SHA512Digest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -13,7 +15,8 @@ public class KeccakTests {
     private static final String testHash = "a34d39c08fc1b584dd8d62a1503125feea102b42f0ffc10d217ac2311065a76979fa42ca81a68fc686a6782affd4fe2967b75789630500750582d049e8a9f424";
     @Test
     public void sha3512() throws Exception{
-        byte[] res = Keccak.Keccak512(Hex.decodeHex(testbuf.toCharArray()));
+        //byte[] res = Keccak.Keccak512(Hex.decodeHex(testbuf.toCharArray()));
+        byte[] res=SHA3Utility.hash(testbuf.getBytes(UTF_8),new SHA512Digest());
         assert Hex.encodeHexString(res).equals(testHash);
     }
 }
