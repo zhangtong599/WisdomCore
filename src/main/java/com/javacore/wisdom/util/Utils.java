d1ed9639446f4d5d75377e0d611a800257fd2e1e
mystudygogo <tnix_blockchain@outlook.com>
2019-03-22 21:05:27 +0800

    Utils

--- src/main/java/com/javacore/wisdom/util/Utils.java
+++ src/main/java/com/javacore/wisdom/util/Utils.java
@@ -1,12 +1,115 @@
 package com.javacore.wisdom.util;
 
-import org.apache.log4j.Logger;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.FileVisitOption;
+
+import com.google.protobuf.ByteString;
+import com.google.protobuf.Timestamp;
+
+import java.security.SecureRandom;
+import java.time.Instant;
+import java.util.UUID;
+
+import java.util.Comparator;
+
+
+import static org.apache.commons.codec.binary.Hex.encodeHexString;
 
 public final class Utils {
-	
-	private  Logger logger=Logger.getLogger(Utils.class);
-	
-	
-	
+
+
+    /**
+     * Read the contents a file.
+     *
+     * @param input source file to read.
+     * @return contents of the file.
+     * @throws IOException
+     */
+    public static byte[] readFile(File input) throws IOException {
+        return Files.readAllBytes(Paths.get(input.getAbsolutePath()));
+    }
+
+    /**
+     * Generate a v4 UUID
+     *
+     * @return String representation of {@link UUID}
+     */
+    public static String generateUUID() {
+        return UUID.randomUUID().toString();
+    }
+
+
+    /**
+     * Create a new {@link Timestamp} instance based on the current time
+     *
+     * @return timestamp
+     */
+    public static Timestamp generateTimestamp() {
+        Instant time = Instant.now();
+        return Timestamp.newBuilder().setSeconds(time.getEpochSecond())
+                .setNanos(time.getNano()).build();
+    }
+
+
+
+    private static final int NONONCE_LENGTH = 32;
+
+    private static final SecureRandom RANDOM = new SecureRandom();
+
+    public static byte[] generateNonce() {
+
+        byte[] values = new byte[NONONCE_LENGTH];
+        RANDOM.nextBytes(values);
+
+        return values;
+    }
+
+    public static String toHexString(ByteString byteString) {
+        if (byteString == null) {
+            return null;
+        }
+
+        return encodeHexString(byteString.toByteArray());
+
+    }
+
+    public static String toHexString(byte[] bytes) {
+        if (bytes == null) {
+            return null;
+        }
+
+        return encodeHexString(bytes);
+
+    }
+
+
+    /**
+     * Delete a file or directory
+     *
+     * @param file {@link File} representing file or directory
+     * @throws IOException
+     */
+    public static void deleteFileOrDirectory(File file) throws IOException {
+        if (file.exists()) {
+            if (file.isDirectory()) {
+                Path rootPath = Paths.get(file.getAbsolutePath());
+
+                Files.walk(rootPath, FileVisitOption.FOLLOW_LINKS)
+                        .sorted(Comparator.reverseOrder())
+                        .map(Path::toFile)
+                        .forEach(File::delete);
+            } else {
+                file.delete();
+            }
+        } else {
+            throw new RuntimeException("File or directory does not exist");
+        }
+    }
+
 
 }
